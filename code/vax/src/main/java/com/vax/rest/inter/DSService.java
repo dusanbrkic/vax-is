94c6d96a00be1715100cf47f17056b69dcbb8704
nadjagv <gvozdenacn@gmail.com>
2022-01-11 03:34:21 +0100

    Metapodaci

--- code/vax/src/main/java/com/vax/rest/inter/DSService.java
+++ code/vax/src/main/java/com/vax/rest/inter/DSService.java
@@ -1,8 +1,12 @@
 package com.vax.rest.inter;
 
+import java.io.IOException;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+
+import org.xml.sax.SAXException;
 
 import proj.xml.gradj.digitalni_sertifikat.DigitalniSertifikat;
 
@@ -11,7 +15,7 @@ public interface DSService {
 	@GET
 	@Path("/testUnmarshal")
 	@Produces("application/xml")
-	public DigitalniSertifikat testUnmarshal();
+	public DigitalniSertifikat testUnmarshal() throws SAXException, IOException;
 	
 	@GET
 	@Path("/testMarshal")
--- code/vax/src/main/java/com/vax/rest/ws/DSServiceImpl.java
+++ code/vax/src/main/java/com/vax/rest/ws/DSServiceImpl.java
@@ -1,5 +1,10 @@
 package com.vax.rest.ws;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.math.BigInteger;
 import java.util.Date;
 import java.util.GregorianCalendar;
@@ -7,10 +12,22 @@ import java.util.GregorianCalendar;
 import javax.ws.rs.Path;
 import javax.xml.datatype.DatatypeConfigurationException;
 import javax.xml.datatype.DatatypeFactory;
+import javax.xml.transform.TransformerException;
 
+import org.apache.jena.query.QueryExecution;
+import org.apache.jena.query.QueryExecutionFactory;
+import org.apache.jena.query.ResultSet;
+import org.apache.jena.query.ResultSetFormatter;
+import org.apache.jena.update.UpdateExecutionFactory;
+import org.apache.jena.update.UpdateFactory;
+import org.apache.jena.update.UpdateProcessor;
+import org.apache.jena.update.UpdateRequest;
 import org.springframework.stereotype.Service;
+import org.xml.sax.SAXException;
 
 import com.vax.rest.inter.DSService;
+import com.vax.rest.util.MetadataExtractor;
+import com.vax.rest.util.SparqlUtil;
 import com.vax.rest.util.XMLDatabase;
 import com.vax.rest.util.XMLParser;
 
@@ -22,11 +39,33 @@ import proj.xml.gradj.digitalni_sertifikat.DigitalniSertifikat.OsnovniPodaci;
 public class DSServiceImpl implements DSService {
 
 	@Override
-	public DigitalniSertifikat testUnmarshal() {
+	public DigitalniSertifikat testUnmarshal() throws SAXException, IOException {
 		System.out.println("Testing unmarshal");
 		
 		DigitalniSertifikat ds = (DigitalniSertifikat) XMLParser.unmarshal("proj.xml.gradj.digitalni_sertifikat", "digitalni_sertifikat.xsd", "digitalni_sertifikat_primer.xml",true,false,null);	
 		System.out.println(ds);	
+		
+		
+		
+		String xmlFilePath = "./src/main/resources/xml/digitalni_sertifikat_primer.xml";
+		
+		String rdfFilePath = "./src/main/resources/rdf/rezultat_test.rdf";
+		MetadataExtractor metadataExtractor = new MetadataExtractor();
+		
+		System.out.println("[INFO] Extracting metadata from RDFa attributes...");
+		try {
+			metadataExtractor.extractMetadata(
+					new FileInputStream(new File(xmlFilePath)), 
+					new FileOutputStream(new File(rdfFilePath)));
+		} catch (TransformerException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+				
+		
+		//upis u bazu
+		
+		
 		return ds;
 	}
 
