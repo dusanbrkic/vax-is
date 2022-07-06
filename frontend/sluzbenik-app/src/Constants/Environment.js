eb360ad255bc4aee2647705dd2f46aeba3f1f1bf
Filip <filip.zivanac@gmail.com>
2022-07-07 01:59:59 +0200

    frontend init

---
+++ frontend/sluzbenik-app/src/Constants/Environment.js
@@ -0,0 +1,4 @@
+const Environment = {
+  baseURL: "https://localhost:8080/",
+};
+export default Environment;
---
+++ frontend/sluzbenik-app/src/Services/AuthService.js
@@ -0,0 +1,22 @@
+import Environment from "../Constants/Environment";
+
+const testUser = { name: "Hajduk", surname: "Dusan" };
+
+const getUser = () => {
+  const user = JSON.parse(sessionStorage.getItem("user"));
+  return user;
+};
+
+const isAuth = () => {
+  const user = JSON.parse(sessionStorage.getItem("user"));
+  return user === null;
+};
+
+const setUser = (user) => {
+  sessionStorage.setItem("user", JSON.stringify(user));
+};
+
+const removeUser = () => {
+  sessionStorage.removeItem("user");
+};
+export default { getUser, setUser, removeUser, isAuth };
